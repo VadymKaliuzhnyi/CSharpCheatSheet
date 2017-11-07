0902f603c82843f32b7f21d52d8def444ecd0770
Vadym <vadym.kaliuzhnyi@yandex.com>
2017-11-07 23:42:27 +0100

    Moved all projects from dots to tabs

--- [C#7.0] [.NET2.0-4.7] [VS17]/[C#7.0] [.NET2.0-4.7] [VS17] DigitSeparators/DigitSeparators/Program.cs
+++ [C#7.0] [.NET2.0-4.7] [VS17]/[C#7.0] [.NET2.0-4.7] [VS17] DigitSeparators/DigitSeparators/Program.cs
@@ -3,52 +3,52 @@ using static System.Console;
 
 namespace DigitSeparators
 {
-    class Program
-    {
-        static void Main()
-        {
-            #region Intro
-            // The underscore _ may be used as a digit separator.
-            // Being able to group digits in large numeric literals has a significant impact on readability.
-            #endregion
+	class Program
+	{
+		static void Main()
+		{
+			#region Intro
+			// The underscore _ may be used as a digit separator.
+			// Being able to group digits in large numeric literals has a significant impact on readability.
+			#endregion
 
-            #region Scope
-            // Separator can be applied to binary, hexadecimal and decimal values.
+			#region Scope
+			// Separator can be applied to binary, hexadecimal and decimal values.
 
-            int binary = 0b1101_1101_0100_0100_0111_0011;
-            int hexadecimal = 0xDD_44_73;
-            int dec = 145_00_97_9;
+			int binary = 0b1101_1101_0100_0100_0111_0011;
+			int hexadecimal = 0xDD_44_73;
+			int dec = 145_00_97_9;
 
-            WriteLine($"0b1101_1101_0100_0100_0111_0011 converted to int is {binary}");
-            WriteLine($"0xDD_44_73 converted to int is {hexadecimal}");
-            WriteLine($"145_00_97_9 converted to inc is {dec}");
-            #endregion
+			WriteLine($"0b1101_1101_0100_0100_0111_0011 converted to int is {binary}");
+			WriteLine($"0xDD_44_73 converted to int is {hexadecimal}");
+			WriteLine($"145_00_97_9 converted to inc is {dec}");
+			#endregion
 
-            #region HowMuchUnderscores?
-            // Different groupings may make sense in different scenarios or with different numeric bases.
-            // Any sequence of digits may be separated by one or more underscores.
-            // The separators have no semantic impact - they are simply ignored.
+			#region HowMuchUnderscores?
+			// Different groupings may make sense in different scenarios or with different numeric bases.
+			// Any sequence of digits may be separated by one or more underscores.
+			// The separators have no semantic impact - they are simply ignored.
 
-            int dec2 = 1_000_000; // Million
-            int dec3 = 1___0_0_0___0_0_0; // Million
+			int dec2 = 1_000_000; // Million
+			int dec3 = 1___0_0_0___0_0_0; // Million
 
-            WriteLine(Environment.NewLine);
-            WriteLine($"1_000_000 is the same million ({dec2}) as 1___0_0_0___0_0_0 ({dec3})");
-            #endregion
+			WriteLine(Environment.NewLine);
+			WriteLine($"1_000_000 is the same million ({dec2}) as 1___0_0_0___0_0_0 ({dec3})");
+			#endregion
 
-            #region Exceptions
-            // The underscore may occur anywhere in a numeric literal but with the below exceptions.
-            // Where the _ digit separator may NOT be used:
+			#region Exceptions
+			// The underscore may occur anywhere in a numeric literal but with the below exceptions.
+			// Where the _ digit separator may NOT be used:
 
-            //int dec4 = _213123; //at the beginning of the value
-            //int dec5 = 36546456_; //at the end of the value
-            //int dec6 = 17_.0; //next to the decimal
-            //int dec7 = 1.1e_1; //next to the exponent character
-            //int dec8 = 10_f; //next to the type specifier
-            //int binary2 = 0b_1001_1000; //immediately following the 0x or 0b in binary and hexadecimal literals
-            #endregion
+			//int dec4 = _213123; //at the beginning of the value
+			//int dec5 = 36546456_; //at the end of the value
+			//int dec6 = 17_.0; //next to the decimal
+			//int dec7 = 1.1e_1; //next to the exponent character
+			//int dec8 = 10_f; //next to the type specifier
+			//int binary2 = 0b_1001_1000; //immediately following the 0x or 0b in binary and hexadecimal literals
+			#endregion
 
-            Console.ReadKey();
-        }
-    }
+			Console.ReadKey();
+		}
+	}
 }
--- [C#7.0] [.NET2.0-4.7] [VS17]/[C#7.0] [.NET2.0-4.7] [VS17] OutVariables/OutVariables/Program.cs
+++ [C#7.0] [.NET2.0-4.7] [VS17]/[C#7.0] [.NET2.0-4.7] [VS17] OutVariables/OutVariables/Program.cs
@@ -4,146 +4,146 @@ using static System.Console;
 
 namespace OutVariables
 {
-    static partial class Program
-    {
-        static void Main()
-        {
-            #region OldStyle
-            // Old C# 6.0 approach
+	static partial class Program
+	{
+		static void Main()
+		{
+			#region OldStyle
+			// Old C# 6.0 approach
 
-            int xOldApproach; // declare the variables here...
-            string yOldApproach; // .. but not initialize them (as you would normally do)
+			int xOldApproach; // declare the variables here...
+			string yOldApproach; // .. but not initialize them (as you would normally do)
 
-            SomeMethod(out xOldApproach, out yOldApproach); // pass variables to the method where they will be initialized
+			SomeMethod(out xOldApproach, out yOldApproach); // pass variables to the method where they will be initialized
 
-            WriteLine($"xOldApproach is: {xOldApproach}\n" + // use variables after initialization and assignment
-                      $"yOldApproach is: {yOldApproach}");
-            #endregion
+			WriteLine($"xOldApproach is: {xOldApproach}\n" + // use variables after initialization and assignment
+					  $"yOldApproach is: {yOldApproach}");
+			#endregion
 
-            #region NewStyle
-            // New C# 7.0 approach - explicit type declaration
+			#region NewStyle
+			// New C# 7.0 approach - explicit type declaration
 
-            SomeMethod(out int xNewApproach, out string yNewApproach); // declare variables in line with passing them
+			SomeMethod(out int xNewApproach, out string yNewApproach); // declare variables in line with passing them
 
-            WriteLine($"xNewApproach is: {xNewApproach}\n" +
-                      $"yNewApproach is: {yNewApproach}");
-            #endregion
+			WriteLine($"xNewApproach is: {xNewApproach}\n" +
+					  $"yNewApproach is: {yNewApproach}");
+			#endregion
 
-            #region NewStyleVar
-            // New C# 7.0 approach - var usage
+			#region NewStyleVar
+			// New C# 7.0 approach - var usage
 
-            SomeMethod(out var xNewVarApproach, out var yNewVarApproach); // declare implicitly typed local variable
+			SomeMethod(out var xNewVarApproach, out var yNewVarApproach); // declare implicitly typed local variable
 
-            WriteLine($"xNewVarApproach is: {xNewApproach}\n" +
-                      $"yNewVarApproach is: {yNewApproach}");
-            #endregion
+			WriteLine($"xNewVarApproach is: {xNewApproach}\n" +
+					  $"yNewVarApproach is: {yNewApproach}");
+			#endregion
 
-            #region NewStyleWildcard
-            // If some of the parameters that a function returns in out is not needed
-            // you can use the discard operator '_'
+			#region NewStyleWildcard
+			// If some of the parameters that a function returns in out is not needed
+			// you can use the discard operator '_'
 
-            SomeMethod(out var xNewWildcardApproach, out var _ ); // I only care about first parameter
+			SomeMethod(out var xNewWildcardApproach, out var _ ); // I only care about first parameter
 
-            WriteLine($"xNewWildcardApproach is: {xNewApproach}");
-            WriteLine(Environment.NewLine);
-            #endregion
+			WriteLine($"xNewWildcardApproach is: {xNewApproach}");
+			WriteLine(Environment.NewLine);
+			#endregion
 
-            #region UseCase
-            // Real world use case
+			#region UseCase
+			// Real world use case
 
-            CanThisStringBeParsedToInt("Here we are passing wrong input");
-            CanThisStringBeParsedToInt("137");
-            #endregion
+			CanThisStringBeParsedToInt("Here we are passing wrong input");
+			CanThisStringBeParsedToInt("137");
+			#endregion
 
-            #region NewStyleWithAnonymousType
-            // new out variables can be used even with anonymous type
+			#region NewStyleWithAnonymousType
+			// new out variables can be used even with anonymous type
 
-            // for the sake of example imagine you are working with some array and running linq thru it
-            var range = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            var groupedByMod2 = range
-                .Select(x => new
-                {
-                    Source = x,
-                    ModBy2 = x % 2
-                })
-                .GroupBy(input => input.ModBy2)
-                .ToDictionary(k => k.Key, v => v.ToArray());
+			// for the sake of example imagine you are working with some array and running linq thru it
+			var range = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			var groupedByMod2 = range
+				.Select(x => new
+				{
+					Source = x,
+					ModBy2 = x % 2
+				})
+				.GroupBy(input => input.ModBy2)
+				.ToDictionary(k => k.Key, v => v.ToArray());
 
-            // Now imagine you want to use TryGetValue method for "groupedByMod2" variable
-            // http://referencesource.microsoft.com/#mscorlib/system/collections/generic/dictionary.cs,2e5bc6d8c0f21e67
-            //public bool TryGetValue(TKey key, out TValue value)
-            //{
-            //    int i = FindEntry(key);
-            //    if (i >= 0)
-            //    {
-            //        value = entries[i].value;
-            //        return true;
-            //    }
-            //    value = default(TValue);
-            //    return false;
-            //}
+			// Now imagine you want to use TryGetValue method for "groupedByMod2" variable
+			// http://referencesource.microsoft.com/#mscorlib/system/collections/generic/dictionary.cs,2e5bc6d8c0f21e67
+			//public bool TryGetValue(TKey key, out TValue value)
+			//{
+			//    int i = FindEntry(key);
+			//    if (i >= 0)
+			//    {
+			//        value = entries[i].value;
+			//        return true;
+			//    }
+			//    value = default(TValue);
+			//    return false;
+			//}
 
-            #region C#6.0
-            // in C# 6.0 you should:
-            // 1. create a variable
-            // 2. of anonymous type
-            // 3. which will exactly match anonymous of your post-linq variable
+			#region C#6.0
+			// in C# 6.0 you should:
+			// 1. create a variable
+			// 2. of anonymous type
+			// 3. which will exactly match anonymous of your post-linq variable
 
-            // System.Collections.Generic.Dictionary<int, <>f__AnonymousType0<int, int>[]>
-            var temp = new[] {
-                                new { Source = 108, ModBy2 = 5 }
-                              };
+			// System.Collections.Generic.Dictionary<int, <>f__AnonymousType0<int, int>[]>
+			var temp = new[] {
+								new { Source = 108, ModBy2 = 5 }
+							  };
 
-            // now you can pass it with "out" modifier to the above mentioned method.
-            if (groupedByMod2.TryGetValue(1, out temp))
-            {
-                WriteLine(temp.Length);
-            }
-            #endregion
+			// now you can pass it with "out" modifier to the above mentioned method.
+			if (groupedByMod2.TryGetValue(1, out temp))
+			{
+				WriteLine(temp.Length);
+			}
+			#endregion
 
-            #region C# 7.0
-            // in C# 7.0 you can just type "out var" and compiler will do everything for you
+			#region C# 7.0
+			// in C# 7.0 you can just type "out var" and compiler will do everything for you
 
-            if (groupedByMod2.TryGetValue(1, out var oddEl))
-            {
-                WriteLine(oddEl.Length);
-            }
-            #endregion
+			if (groupedByMod2.TryGetValue(1, out var oddEl))
+			{
+				WriteLine(oddEl.Length);
+			}
+			#endregion
 
-            #endregion
+			#endregion
 
-            #region NewStyleLimitations
-            // In current version of C# (7.0) out var declarations are of limited use in LINQ queries.
-            // Since expressions are interpreted as expression lambdas,
-            // so the scope of the input variables is limited to these lambdas.
+			#region NewStyleLimitations
+			// In current version of C# (7.0) out var declarations are of limited use in LINQ queries.
+			// Since expressions are interpreted as expression lambdas,
+			// so the scope of the input variables is limited to these lambdas.
 
-            var SomeRange = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			var SomeRange = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            // var CanItBeParsedToInt =
-            // from item in SomeRange
+			// var CanItBeParsedToInt =
+			// from item in SomeRange
 
-            // let result = int.TryParse(item.ToString(), out var tmp)
-            //// "Out variable and pattern variable declarations are not allowed within a query clause."
+			// let result = int.TryParse(item.ToString(), out var tmp)
+			//// "Out variable and pattern variable declarations are not allowed within a query clause."
 
-            // select result ? tmp : 0;
-            ///// "The name does not exist in the current context"
+			// select result ? tmp : 0;
+			///// "The name does not exist in the current context"
 
-            // some developers notes:
-            // https://github.com/dotnet/csharplang/blob/master/meetings/2016/LDM-2016-12-07-14.md#expression-variables-in-query-expressions
-            // We won't have time to do this feature in C# 7.0.
-            // If we want to leave ourselves room to do it in the future, we need to make sure
-            // that we don't allow expression variables in query clauses to mean something else today,
-            // that would contradict such a future.
-            // The current semantics is that expression variables in query clauses are scoped
-            // to only the query clause.That means two subsequent query clauses can use the same
-            // name in expression variables, for instance.That is inconsistent with a future that
-            // allows those variables to share a scope across query clause boundaries.
-            // Thus, if we want to allow this in the future we have to put in some restrictions
-            // in C# 7.0 to protect the design space.
+			// some developers notes:
+			// https://github.com/dotnet/csharplang/blob/master/meetings/2016/LDM-2016-12-07-14.md#expression-variables-in-query-expressions
+			// We won't have time to do this feature in C# 7.0.
+			// If we want to leave ourselves room to do it in the future, we need to make sure
+			// that we don't allow expression variables in query clauses to mean something else today,
+			// that would contradict such a future.
+			// The current semantics is that expression variables in query clauses are scoped
+			// to only the query clause.That means two subsequent query clauses can use the same
+			// name in expression variables, for instance.That is inconsistent with a future that
+			// allows those variables to share a scope across query clause boundaries.
+			// Thus, if we want to allow this in the future we have to put in some restrictions
+			// in C# 7.0 to protect the design space.
 
-            #endregion
+			#endregion
 
-            ReadKey();
-        }
-    }
+			ReadKey();
+		}
+	}
 }
